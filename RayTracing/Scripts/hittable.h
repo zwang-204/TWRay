0eed348c091c9178a3aee9ee81b928f10cd7332f
GameTechArt-Tony <79949768+zwang-204@users.noreply.github.com>
2022-10-19 19:15:09 +0800

    Add files via upload

---
+++ RayTracing/Scripts/hittable.h
@@ -0,0 +1,183 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "pbrt.h"
+#include "geometry.h"
+
+namespace pbrt{
+
+class material;
+
+struct hit_record {
+    Point3f p;
+    Vector3f normal;
+    shared_ptr<material> mat_ptr;
+    float t;
+    float u;
+    float v;
+    bool front_face;
+
+    inline void set_face_normal(const Ray& r, const Vector3f& outward_normal){
+        front_face = Dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class hittable {
+    public:
+        virtual bool hit(const Ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+        virtual bool bounding_box(float time0, float time1, Bounds3f& output_box) const = 0;
+        virtual float pdf_value(const Point3f& o, const Vector3f& v) const {
+            return 0.0;
+        }
+
+        virtual Vector3f random(const Point3f& o) const {
+            return Vector3f(1, 0, 0);
+        }
+};
+
+class translate: public hittable {
+    public:
+        translate(shared_ptr<hittable> p, const Vector3f& displacement)
+            : ptr(p), offset(displacement) {}
+
+        virtual bool hit(
+            const Ray& r, float t_min, float t_max, hit_record& rec) const override;
+        
+        virtual bool bounding_box(float time0, float  time1, Bounds3f& output_box) const override;
+
+    public:
+        shared_ptr<hittable> ptr;
+        Vector3f offset;
+};
+
+bool translate::hit(const Ray& r, float t_min, float t_max, hit_record& rec) const {
+    Ray moved_r(r.origin() - offset, r.direction());
+    if (!ptr->hit(moved_r, t_min, t_max, rec))
+        return false;
+    
+    rec.p += offset;
+    rec.set_face_normal(moved_r, rec.normal);
+
+    return true;
+}
+
+bool translate::bounding_box(float time0, float time1, Bounds3f& output_box) const {
+    // infinite recursion?
+    if(!ptr->bounding_box(time0, time1, output_box))
+        return false;
+    
+    output_box = Bounds3f(
+        output_box.Min() + offset,
+        output_box.Max() + offset);
+    
+    return true;
+}
+
+class rotate_y : public hittable {
+    public: 
+        rotate_y(shared_ptr<hittable> p, float angle);
+
+        virtual bool hit(
+            const Ray& r, float t_min, float t_max, hit_record& rec) const override;
+
+        virtual bool bounding_box(float time0, float time1, Bounds3f& output_box) const override {
+            output_box = bbox;
+            return hasbox;
+        }
+    public:
+        shared_ptr<hittable> ptr;
+        float sin_theta;
+        float cos_theta;
+        bool hasbox;
+        Bounds3f bbox;
+};
+
+rotate_y::rotate_y(shared_ptr<hittable> p, float angle) : ptr(p) {
+    auto radians = Radians(angle);
+    sin_theta = sin(radians);
+    cos_theta = cos(radians);
+    hasbox = ptr->bounding_box(0, 1, bbox);
+
+    Point3f min(Infinity, Infinity, Infinity);
+    Point3f max(-Infinity, -Infinity, -Infinity);
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            for(int k = 0; k < 2; k++){
+                auto x = i*bbox.Max().x + (1-i)*bbox.Min().x;
+                auto y = j*bbox.Max().y + (1-j)*bbox.Min().y;
+                auto z = k*bbox.Max().z + (1-k)*bbox.Min().z;
+
+                auto newx = cos_theta*x + sin_theta*z;
+                auto newz = -sin_theta*x + cos_theta*z;
+
+                Vector3f tester(newx, y, newz);
+                
+                for(int c = 0; c < 3; c++){
+                    min[c] = fminf(min[c], tester[c]);
+                    max[c] = fmaxf(max[c], tester[c]);
+                }
+            }
+        }
+
+        bbox = Bounds3f(min, max);
+    }
+}
+
+bool rotate_y::hit(const Ray& r, float t_min, float t_max, hit_record& rec) const {
+    auto origin = r.origin();
+    auto direction = r.direction();
+
+    origin[0] = cos_theta*r.origin()[0] - sin_theta*r.origin()[2]; 
+    origin[2] = sin_theta*r.origin()[0] + cos_theta*r.origin()[2];
+
+    direction[0] = cos_theta*r.direction()[0] - sin_theta*r.direction()[2]; 
+    direction[2] = sin_theta*r.direction()[0] + cos_theta*r.direction()[2];
+
+    Ray rotated_r(origin, direction);
+
+    if(!ptr->hit(rotated_r, t_min, t_max, rec))
+        return false;
+    
+    auto p = rec.p;
+    auto normal = rec.normal;
+
+    p[0] = cos_theta*rec.p[0] + sin_theta*rec.p[2];
+    p[2] = -sin_theta*rec.p[0] + cos_theta*rec.p[2];
+
+    normal[0] = cos_theta*rec.normal[0] + sin_theta*rec.normal[2];
+    normal[2] = -sin_theta*rec.normal[0] + cos_theta*rec.normal[2];
+
+    rec.p = p;
+    rec.set_face_normal(rotated_r, normal);
+
+    return true;
+}
+
+class flip_face : public hittable {
+    public:
+        flip_face(shared_ptr<hittable> p) : ptr(p) {}
+
+        virtual bool hit(
+            const Ray& r, float t_min, float t_max, hit_record& rec) const override{
+            
+            if (!ptr->hit(r, t_min, t_max, rec))
+                return false;
+            
+            rec.front_face = !rec.front_face;
+            return true;
+
+        }
+
+        virtual bool bounding_box(float time0, float time1, Bounds3f& output_box) const override {
+            return ptr->bounding_box(time0, time1, output_box);
+        }
+    
+    public:
+        shared_ptr<hittable> ptr;
+};
+
+}
+
+#endif
---
+++ RayTracing/Scripts/vec3pre.h
@@ -0,0 +1,236 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+#include "pbrt.h"
+
+using std::sqrt;
+
+template <typename T>
+inline bool isNaN(const T x) {
+    return std::isnan(x);
+}
+template <>
+inline bool isNaN(const int x) {
+    return false;
+}
+
+// Vector Declarations
+template <typename T>
+class Vector2 {
+  public:
+    // Vector2 Public Methods
+    Vector2() { x = y = 0; }
+    Vector2(T xx, T yy) : x(xx), y(yy) { DCHECK(!HasNaNs()); }
+    bool HasNaNs() const { return isNaN(x) || isNaN(y); }
+    explicit Vector2(const Point2<T> &p);
+    explicit Vector2(const Point3<T> &p);
+#ifndef NDEBUG
+    // The default versions of these are fine for release builds; for debug
+    // we define them so that we can add the Assert checks.
+    Vector2(const Vector2<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x = v.x;
+        y = v.y;
+    }
+    Vector2<T> &operator=(const Vector2<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x = v.x;
+        y = v.y;
+        return *this;
+    }
+#endif  // !NDEBUG
+
+    Vector2<T> operator+(const Vector2<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Vector2(x + v.x, y + v.y);
+    }
+
+    Vector2<T> &operator+=(const Vector2<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+    Vector2<T> operator-(const Vector2<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Vector2(x - v.x, y - v.y);
+    }
+
+    Vector2<T> &operator-=(const Vector2<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x -= v.x;
+        y -= v.y;
+        return *this;
+    }
+    bool operator==(const Vector2<T> &v) const { return x == v.x && y == v.y; }
+    bool operator!=(const Vector2<T> &v) const { return x != v.x || y != v.y; }
+    template <typename U>
+    Vector2<T> operator*(U f) const {
+        return Vector2<T>(f * x, f * y);
+    }
+
+    template <typename U>
+    Vector2<T> &operator*=(U f) {
+        DCHECK(!isNaN(f));
+        x *= f;
+        y *= f;
+        return *this;
+    }
+    template <typename U>
+    Vector2<T> operator/(U f) const {
+        CHECK_NE(f, 0);
+        Float inv = (Float)1 / f;
+        return Vector2<T>(x * inv, y * inv);
+    }
+
+    template <typename U>
+    Vector2<T> &operator/=(U f) {
+        CHECK_NE(f, 0);
+        Float inv = (Float)1 / f;
+        x *= inv;
+        y *= inv;
+        return *this;
+    }
+    Vector2<T> operator-() const { return Vector2<T>(-x, -y); }
+    T operator[](int i) const {
+        DCHECK(i >= 0 && i <= 1);
+        if (i == 0) return x;
+        return y;
+    }
+
+    T &operator[](int i) {
+        DCHECK(i >= 0 && i <= 1);
+        if (i == 0) return x;
+        return y;
+    }
+    Float LengthSquared() const { return x * x + y * y; }
+    Float Length() const { return std::sqrt(LengthSquared()); }
+
+    // Vector2 Public Data
+    T x, y;
+};
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &os, const Vector2<T> &v) {
+    os << "[ " << v.x << ", " << v.y << " ]";
+    return os;
+}
+
+template <typename T>
+class Vector3 {
+  public:
+    // Vector3 Public Methods
+    T operator[](int i) const {
+        DCHECK(i >= 0 && i <= 2);
+        if (i == 0) return x;
+        if (i == 1) return y;
+        return z;
+    }
+    T &operator[](int i) {
+        DCHECK(i >= 0 && i <= 2);
+        if (i == 0) return x;
+        if (i == 1) return y;
+        return z;
+    }
+    Vector3() { x = y = z = 0; }
+    Vector3(T x, T y, T z) : x(x), y(y), z(z) { DCHECK(!HasNaNs()); }
+    bool HasNaNs() const { return isNaN(x) || isNaN(y) || isNaN(z); }
+    explicit Vector3(const Point3<T> &p);
+#ifndef NDEBUG
+    // The default versions of these are fine for release builds; for debug
+    // we define them so that we can add the Assert checks.
+    Vector3(const Vector3<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x = v.x;
+        y = v.y;
+        z = v.z;
+    }
+
+    Vector3<T> &operator=(const Vector3<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x = v.x;
+        y = v.y;
+        z = v.z;
+        return *this;
+    }
+#endif  // !NDEBUG
+    Vector3<T> operator+(const Vector3<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Vector3(x + v.x, y + v.y, z + v.z);
+    }
+    Vector3<T> &operator+=(const Vector3<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+    Vector3<T> operator-(const Vector3<T> &v) const {
+        DCHECK(!v.HasNaNs());
+        return Vector3(x - v.x, y - v.y, z - v.z);
+    }
+    Vector3<T> &operator-=(const Vector3<T> &v) {
+        DCHECK(!v.HasNaNs());
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+    bool operator==(const Vector3<T> &v) const {
+        return x == v.x && y == v.y && z == v.z;
+    }
+    bool operator!=(const Vector3<T> &v) const {
+        return x != v.x || y != v.y || z != v.z;
+    }
+    template <typename U>
+    Vector3<T> operator*(U s) const {
+        return Vector3<T>(s * x, s * y, s * z);
+    }
+    template <typename U>
+    Vector3<T> &operator*=(U s) {
+        DCHECK(!isNaN(s));
+        x *= s;
+        y *= s;
+        z *= s;
+        return *this;
+    }
+    template <typename U>
+    Vector3<T> operator/(U f) const {
+        CHECK_NE(f, 0);
+        Float inv = (Float)1 / f;
+        return Vector3<T>(x * inv, y * inv, z * inv);
+    }
+
+    template <typename U>
+    Vector3<T> &operator/=(U f) {
+        CHECK_NE(f, 0);
+        Float inv = (Float)1 / f;
+        x *= inv;
+        y *= inv;
+        z *= inv;
+        return *this;
+    }
+    Vector3<T> operator-() const { return Vector3<T>(-x, -y, -z); }
+    Float LengthSquared() const { return x * x + y * y + z * z; }
+    Float Length() const { return std::sqrt(LengthSquared()); }
+    explicit Vector3(const Normal3<T> &n);
+
+    // Vector3 Public Data
+    T x, y, z;
+};
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &os, const Vector3<T> &v) {
+    os << "[ " << v.x << ", " << v.y << ", " << v.z << " ]";
+    return os;
+}
+
+typedef Vector2<float> Vector2f;
+typedef Vector2<int> Vector2i;
+typedef Vector3<float> Vector3f;
+typedef Vector3<int> Vector3i;
+
+#endif
